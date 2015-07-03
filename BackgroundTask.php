50c2610daa76ae6f72ab46a846f5d5d250f230e9
Alexey Baranov <baranov.alxy@gmail.com>
2015-07-03 17:40:03 +0600

    #3104: refactoring

--- BackgroundTask.php
+++ BackgroundTask.php
@@ -1,21 +1,24 @@
 <?php
 
-namespace idfly\components;
+namespace idfly;
 
-class BackgroundTask
+abstract class BackgroundTask
 {
 
     public static function run($task, $options = [])
     {
-        if(isset($options['concurrent']) &&
-            $options['concurrent'] === true) {
+        $isConcurrentRun =
+            isset($options['concurrent']) &&
+            $options['concurrent'] === true;
+
+        if($isConcurrentRun) {
             return BackgroundTask::_concurrentRun($task, $options);
         }
 
         return BackgroundTask::_blockingRun($task, $options);
     }
 
-    static function _concurrentRun($task, $options)
+    protected static function _concurrentRun($task, $options)
     {
         $taskDir = BackgroundTask::_getTaskDir($task);
 
@@ -25,116 +28,150 @@ class BackgroundTask
 
         $taskId = BackgroundTask::_generateTaskId();
 
-        $taskFile = $taskDir . '/' . $taskId;
-
         $logFile = BackgroundTask::_getLogFile($task, $taskId);
         $statusFile = BackgroundTask::_getStatusFile($task, $taskId);
 
-        $yiiCmd = BackgroundTask::_getYiiCommand($task, $options);
+        $yiiCmd = BackgroundTask::_getYiiCommandShellSafe($task, $options);
 
-        $cmd = "flock -s $taskDir bash -c " .
-            "'echo > $logFile && echo > $statusFile && " .
-            "$yiiCmd > $logFile 2>&1; echo $? > $statusFile '" .
-            ">/dev/null 2>/dev/null &";
+        $flock = 'flock -s ' . escapeshellarg($taskDir) . ' bash -c';
+
+        $flockCmd = escapeshellarg(
+            "echo > " . escapeshellarg($logFile) . " && " .
+            "echo > " . escapeshellarg($statusFile) . " && " .
+            "$yiiCmd > " . escapeshellarg($logFile) ." 2>&1; " .
+            "echo $? > " . escapeshellarg($statusFile)
+        );
+
+        $cmd = "$flock $flockCmd" . ">/dev/null 2>/dev/null &";
 
         exec($cmd);
 
         return $taskId;
     }
 
-    static function _blockingRun($task, $options)
+    protected static function _blockingRun($task, $options)
     {
-        $yiiCmd = BackgroundTask::_getYiiCommand($task, $options);
+        $yiiCmd = BackgroundTask::_getYiiCommandShellSafe($task, $options);
 
-        $taskFile = BackgroundTask::_getTaskFile($task);
+        $lockFile =  BackgroundTask::_getLockFile($task);
         $logFile = BackgroundTask::_getLogFile($task);
-
         $statusFile = BackgroundTask::_getStatusFile($task);
 
-        $lockFile =  $taskFile . '.lock';
+        $flock = 'flock -n ' . escapeshellarg($lockFile) . ' bash -c';
 
-        $cmd = "flock -n $lockFile bash -c " .
-            "'echo > $logFile && echo > $statusFile && " .
-            "$yiiCmd > $logFile 2>&1; echo $? > $statusFile '" .
-            ">/dev/null 2>/dev/null &";
+        $needToAppendToLogs =
+            isset($options['appendToLogs']) &&
+            $options['appendToLogs'] === true;
+
+        if($needToAppendToLogs) {
+            exec("echo `date` >> " . escapeshellarg($logFile));
+            $yiiCmd .= '>';
+        }
+
+        $flockCmd = escapeshellarg(
+            "echo > " . escapeshellarg($statusFile) . " && " .
+            "$yiiCmd> " . escapeshellarg($logFile) ." 2>&1; " .
+            "echo $? > " . escapeshellarg($statusFile)
+        );
+
+        $cmd = "$flock $flockCmd" . ">/dev/null 2>/dev/null &";
 
         return exec($cmd);
     }
 
-    public static function getTaskProcessesCount($task)
+    public static function getInfo($task, $taskId = null)
     {
-        $taskDir = BackgroundTask::_getTaskDir($task);
-
-        return exec("lsof $taskDir | grep flock | wc -l");
-    }
-
-    public static function getTaskLogs($task, $taskId = null)
-    {
+        $info = [];
         $logFile = BackgroundTask::_getLogFile($task, $taskId);
 
-        if(!file_exists($logFile)) {
-            return false;
+        if(file_exists($logFile)) {
+            $info['log'] = file_get_contents($logFile);
         }
 
-        return file_get_contents($logFile);
-    }
-
-    public static function getTaskStatus($task, $taskId = null)
-    {
         $statusFile = BackgroundTask::_getStatusFile($task, $taskId);
 
-        $code = exec("cat $statusFile");
-
-        if($code === '') {
-            return 'running';
+        if(file_exists($statusFile)) {
+            $info['status_code'] = exec("cat $statusFile");
         }
 
-        if($code === '0') {
-            return 'completed';
+        if(!$taskId) {
+            $lockFile =  BackgroundTask::_getLockFile($task);
+
+            if(file_exists($lockFile)) {
+                $isTaskRunning =
+                    exec("lsof $lockFile | grep flock | wc -l") === '1';
+
+                $info['is_running'] = $isTaskRunning;
+            }
+        } else {
+            $taskDir = BackgroundTask::_getTaskDir($task);
+
+            $isTaskExists =
+                file_exists($logFile) &&
+                file_exists($statusFile);
+
+            if($isTaskExists) {
+                $info['processes_count'] =
+                    exec("lsof $taskDir | grep flock | wc -l");
+            }
         }
 
-        return "error code $code";
+        return $info;
     }
 
-    static function _getYiiCommand($task, $options)
+    protected static function _getYiiCommandShellSafe($task, $options)
     {
         $php = 'php';
         $timeout = '';
 
-        if(isset($options['mem_limit'])) {
-            $php .= ' -d memory_limit=' . escapeshellarg($options['mem_limit']);
+        if(isset($options['memoryLimit'])) {
+            $php .= ' -d memory_limit=' .
+                escapeshellarg($options['memoryLimit']);
         }
 
         if(isset($options['timeout'])) {
             $timeout = 'timeout ' . escapeshellarg($options['timeout']);
         }
 
+        $isValidArgs =
+            isset($options['args']) &&
+            is_array($options['args']);
+
+        $args = '';
+        if($isValidArgs) {
+            foreach($options['args'] as $argument) {
+                $args .= ' ' . escapeshellarg($argument);
+            }
+        }
+
         $yii = \yii::getAlias('@app/yii');
 
-        return "$timeout $php $yii " . escapeshellarg($task);
+        return "$timeout $php $yii " . escapeshellarg($task) . " $args";
     }
 
-    static function _getTaskDir($task)
+    protected static function _getTaskDir($task)
     {
-        return BackgroundTask::_getTaskFile($task);
+        return BackgroundTask::_getTaskFile($task) . '.async';
     }
 
-    static function _getTasksPath()
+    protected static function _getTasksPath()
     {
         return \yii::getAlias('@app/runtime/tasks');
     }
 
-    static function _getTaskFile($task)
+    protected static function _getTaskFile($task)
     {
-        return BackgroundTask::_getTasksPath() . '/' . md5($task);
+        return
+            BackgroundTask::_getTasksPath() . '/' .
+            str_replace('/', '--', $task);
     }
 
-    static function _generateTaskId()
+    protected static function _generateTaskId()
     {
         return md5(microtime(true));
     }
 
-    static function _getStatusFile($task, $taskId = null)
+    protected static function _getStatusFile($task, $taskId = null)
     {
         if($taskId === null) {
             return BackgroundTask::_getTaskFile($task) . '.status';
@@ -147,7 +184,7 @@ class BackgroundTask
         return $statusFile;
     }
 
-    static function _getLogFile($task, $taskId = null)
+    protected static function _getLogFile($task, $taskId = null)
     {
         if($taskId === null) {
             return BackgroundTask::_getTaskFile($task) . '.log';
@@ -158,5 +195,10 @@ class BackgroundTask
         $logFile = $taskFile . '.log';
 
         return $logFile;
+    }
+
+    protected static function _getLockFile($task)
+    {
+        return BackgroundTask::_getTaskFile($task) . '.lock';
     }
 }
